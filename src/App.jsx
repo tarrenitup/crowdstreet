aab6c683087037a139756905743a78a7cd5807b9
Tarren Engberg <tarren@outlook.com>
2019-11-09 20:45:40 -0800

    Fill out components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
 import React from 'react';
 import { StateProvider, useStateValue } from './state.js';
 
-const updateObjectInArray = (array, action) => array.map((item, index) => index !== action.index ? item : { ...item, ...action.item });
-
-const getCells = (start, increment, end) => {
-    if (!start || !increment || !end) return;
-    const length = (end - start) / increment + 1;
-    return new Array(length).fill(0).map((c, i) => start + i * increment);
-}
-
-const Row = ({  }) => (
-    <tr>
-
-    </tr>
+const updateObjectInArray = (array, action) => array.map(
+    (item, idx) => idx !== action.index 
+    ? item 
+    : { ...item, ...action.item }
 );
 
+const isEven = n => (n % 2) === 0;
+
+// factor out
+const getCells = (start, increment, end) => {
+    if (!start || !increment || !end) return;
+    const cellCount = (end - start) / increment + 1;
+    return new Array(cellCount).fill(0).map((c, i) => start + i * increment);
+}
+
+// factor out
+const getRows = (cells, colCount, ltr) => {
+    if (!cells || !colCount ) return;
+    const rowCount = Math.ceil(cells.length / colCount);
+    return new Array(rowCount).fill(0).map((r, i) => {
+        const row = cells.slice(i, i + colCount);
+        const firstColEven = ltr ? isEven(i) : !isEven(i);
+        return firstColEven ? row : row.reverse(); 
+    })
+}
+
+const Row = ({ rowCells }) => (
+    <tr>
+        {rowCells.map(cell => (
+            <td>{cell}</td>
+        ))}
+    </tr>
+)
+
 const Table = ({ table }) => {
-    const rows = 5;
-    // N: start number, X: increment by, M: max/stop number, W: width of table, D: start direction.
-    const cells = getCells(table.N, table.X, table.M,);
+    const colCount = 5;
+    const cells = getCells(table.N, table.X, table.M);
+    const rows = getRows(cells, colCount, table.D_ltr);
 
     return (
         <div className={`table-outer ${table.name}`}>
-            <table style={{width: table.W+'%'}}>
-
+            <table style={{width: `${table.W}%`}}>
+                {rows.map(rowCells => {
+                    <Row rowCells={rowCells} />
+                })}
             </table>
             <div className='controls'>
                 <button onClick={console.log('clicked!')} />
@@ -55,18 +77,18 @@ const Panel = () => (
     </section>
 ) 
 
-const directions = [
-    {label: 'LTR-UP'},
-    {label: 'RTL-UP'},
-    {label: 'LTR-DOWN'},
-    {label: 'RTL-DOWN'},
-]
+// const directions = [
+//     {label: 'LTR-UP', ltr: true, up: true},
+//     {label: 'RTL-UP', ltr: false, up: true},
+//     {label: 'LTR-DOWN', ltr: true, up: false},
+//     {label: 'RTL-DOWN', ltr: false, up: false},
+// ]
 
 const initialState = {
     tables: [
-        {name: 'red', N: 8, X: 1, M: 29, W: 20, D: 0,},
-        {name: 'green', N: 231, X: 1, M: 247, W: 30, D: 0,},
-        {name: 'blue', N: 47, X: 2, M: 81, W: 40, D: 0,},
+        {name: 'red', N: 8, X: 1, M: 29, W: 20, D_ltr: true, D_up: true,},
+        {name: 'green', N: 231, X: 1, M: 247, W: 30, D_ltr: true, D_up: true,},
+        {name: 'blue', N: 47, X: 2, M: 81, W: 40, D_ltr: false, D_up: true,},
     ]
 };
 
